2bae7dc46ca8ff0c4482e5dd6b1df255ed345647
Miguel Bonilla <bonilla7710@gmail.com>
2019-03-13 15:30:50 -0400

    variable name change and fixed more bugs

--- parseconfig.pl
+++ parseconfig.pl
@@ -13,9 +13,9 @@ my (
     @ConfigList_unfiltered,
     $mainDirectory,
     $base_dir,
-    $device_count,
-    $filtered_list,
-    $device_pattern,
+    $config_count,
+    $print_to_file,
+    $config_file_pattern,
     @config_match,
     @config_all_list,
     $parse_external_file,
@@ -30,8 +30,8 @@ $customer_name = `ls /home/cvs/customers/`;
 $customer_name =~ s/\s+$//;
 
 # script arguments
-GetOptions('r|regex=s'          => \$device_pattern,
-           'c|create'           => \$filtered_list,
+GetOptions('r|regex=s'          => \$config_file_pattern,
+           'c|create'           => \$print_to_file,
            'p|parse=s'          => \$parse_external_file,
            'f|file=s'           => \$config_list_file,
            'l|list'             => \$list_dir_pattern,
@@ -54,50 +54,67 @@ sub help {
     print "  $0 -a|--all\n";
     print "\n* Create a file with a list of configurations, generated after regular expression\n";
     print "  $0 -r|--regex <regex> --create|-c\n";
-    print "\n* Parse configuration from file, the file must include objects\n";
+    print "\n* Open and parse configuration from file, the file must include objects\n";
     print "    that have the exact name as configurations stored by rancid\n";
     print "  $0 -f|--file <file_name> -p|--parse [regex1] [regex2] [regex(n)]\n";
+    print "\n* Parse regex in config files generated from the list of devices that\n";
+    print "    your pattern\n";
+    print "  $0 -r|--regex <regex> -p|--parse [regex1] [regex2]...\n";
     print "\n=============================================\n\n";
+}
+sub generate_list_from_pattern {
+    my $configName;
+    foreach $configName (@ConfigList_unfiltered) {
+        if ($configName =~ /$config_file_pattern/gi) {
+            # remove ',v' from the end of the config file
+            push(@config_match, $configName);
+            $config_count++;
+        }
+    }
 }
 
 sub print_pattern_match {
     # initializing local variables to this function
     my $configName;
-    my $device_count;
-    if ($device_pattern) {
+    my $config_count;
+    if ($config_file_pattern) {
         foreach $configName (@ConfigList_unfiltered) {
-            if ($configName =~ /$device_pattern/gi) {
+            if ($configName =~ /$config_file_pattern/gi) {
                 # remove ',v' from the end of the config file
                 $configName =~ s/,v$//;
                 push(@config_match, $configName);
-                $device_count++;
+                $config_count++;
             }
         }
     }
-    if ($device_count > 0){
+    if ($config_count > 0){
         print "\nLISTING CONFIGURATIONS...\n\n";
         for my $line (@config_match) {
             printf " %s",$line;
         }
-        printf "\nYOUR PATTERN MATCHES: %d CONFIGURATION FILES\n",$device_count;
+        printf "\nYOUR PATTERN MATCHES: %d CONFIGURATION FILES\n",$config_count;
     } else {
-        print "\nUNFORTUNATELY YOUR PATTERN DIDNT MATCH ANY FILE\n";
+        print "\nUNFORTUNATELY YOUR PATTERN DIDN'T MATCH ANY FILE\n";
     }
 }
 
-sub print_all {
+sub print_all_clean {
     my $configName;
-    my $device_count;
-    while (<@ConfigList_unfiltered>) {
-        $_ =~ s/,v$//;
-        push(@config_all_list, $_);
-        $device_count ++;
+    my $config_count;
+    foreach my $line (@ConfigList_unfiltered) {
+        $line =~ s/,v$//;
+        push(@config_all_list, $line);
+        $config_count ++;
     }
-    printf "\nLISTING CONFIGURATIONS...\n\n";
-    for my $line (@config_all_list) {
-        printf "%s",$line;
+    if ($config_count > 0){
+         printf "\nLISTING CONFIGURATIONS...\n\n";
+        for my $line (@config_all_list) {
+            printf " %s",$line;
+        }
+        printf "\nYOUR PATTERN MATCHES: %d CONFIGURATION FILES\n",$config_count;
+    } else {
+        print "\nUNFORTUNATELY YOUR PATTERN DIDN'T MATCH ANY FILE\n";
     }
-    printf "\nYOUR PATTERN MATCHES: %d CONFIGURATION FILES\n",$device_count;
 }
 
 sub parse_external_file {
@@ -114,6 +131,7 @@ sub print_to_file {
     #initializing local variables
     my @list_to_print;
     my $filename = $customer_name."_filteredList.txt";
+    generate_list_from_pattern();
     open(FH, '>', $filename) 
     or die "I could not create file '$filename': $!";
     if (@config_match) {
@@ -138,26 +156,25 @@ sub open_config_list {
     }
 }
 
-sub open_config{
-    # open configuration file for reading
+sub open_config {
+    
     
 }
 
 # set the search according to device_pattern entered, if no patter was entered
 # then parse the whole directory content
-if ($device_pattern && $list_dir_pattern) {
+if ($config_file_pattern && $list_dir_pattern) {
     print_pattern_match();
-} elsif ($device_pattern && $filtered_list) {
+} elsif ($config_file_pattern && $print_to_file) {
     print_to_file();
 } elsif ($parse_external_file) {
     parse_external_file($parse_external_file);
 } elsif ($config_list_file) {
     open_config_list();
-} elsif ($list_directory && $filtered_list) {
+} elsif ($list_directory && $print_to_file) {
     print_to_file();
-}
-elsif($list_directory) {
-    print_all();
+} elsif($list_directory && $list_dir_pattern) {
+    print_all_clean();
 } else {
     help();
 }
